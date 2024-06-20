8df31f971239b4e95f073cc711dc24500c524ca7
bluthund <do.it@live.it>
2024-06-21 02:28:00 +0530

    V2.1

--- bin/app.js
+++ bin/app.js
@@ -1,35 +1,41 @@
-var cdres = {
-    qlist: {
-        qcode: new Array(),
-        qdesc: new Array()
-    },
-    score: new Array(),
-    alist: {
-        arand: new Array(),
-        anset: new Array(),
-        ansrt: new Array(), 
-        ansel: new Array()
-    }
+const cdres = {
+    qlist: { qdisc: [], qcode: [], qdesc: [] },
+    score: [],
+    alist: { arand: [], anset: [], ansrt: [], ansel: [] }
 };
 
 const optnum = 4;
-const url = './data/AV01_TPDB.db'; // Replace with your database URL
-const query = `SELECT QCODE,QDESC,ANSET,ANSRT FROM QBANK WHERE QDISC IN ('GEN') ORDER BY RANDOM() LIMIT 10`;
+const cdisc = "CHEM";
+const url = "./data/AV01_TPDB.db";
+const key = "some-AES-key-here";
+const queries = [
+    `SELECT QCODE,QDISC,QDESC,ANSET,ANSRT FROM QBANK WHERE QDISC IN ('GEN') ORDER BY RANDOM() LIMIT 10`,
+    `SELECT QCODE,QDISC,QDESC,ANSET,ANSRT FROM QBANK WHERE QDISC IN ('${cdisc}') ORDER BY RANDOM() LIMIT 10`
+];
 
-let db;
-fetch(url).then(res => res.arrayBuffer()).then(data => {
-    return initSqlJs().then(SQL => {
-        db = new SQL.Database(new Uint8Array(data));
-    });
-}).catch(err => console.error(err));
-
-console.log(db);
-
-// Function to execute the query from the textbox
-function executeQuery() {
+async function initDB() {
     try {
-        const results = db.exec(query);
-        populateTP(results);
+        const res = await fetch(url);
+        const encData = await res.arrayBuffer();
+        const SQL = await initSqlJs();
+        const decData = await decryptDB(encData,key);
+        const results = new Array();
+        queries.forEach(query => {
+            results.push(executeQuery(new SQL.Database(new Uint8Array(decData)),query));
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function decryptDB(data,key) {
+    return data;
+}
+
+async function executeQuery(db,query) {
+    try {
+        const result = db.exec(query);
+        return populateTP(result);
     } catch (err) {
         console.error(err);
         alert('Error executing query: ' + err.message);
@@ -37,42 +43,50 @@ function executeQuery() {
 }
 
 function populateTP(results) {
-    var optnum = 4;
-    results[0].columns.forEach((col,cdx) => {
-        if (col === 'QCODE') {
-            results[0].values.forEach((row,rdx) => {
-                cdres.qlist.qcode.push(row[cdx]);
-                cdres.score.push(0);
-            });
-        }
-        if (col === 'QDESC') {
-            results[0].values.forEach((row,rdx) => {
-                cdres.qlist.qdesc.push(row[cdx]);
-            });
-        }
-        if (col === 'ANSET') {
-            results[0].values.forEach((row,rdx) => {
-                cdres.alist.anset.push(row[cdx].replace(/",/g,'---').split('---'));
-                cdres.alist.arand.push(Array.from({length:optnum},() => Math.floor(Math.random()*1000)));
-                cdres.alist.ansel.push(Array.from({length:optnum},() => 0));
-
-            });
-        }
-        if (col === 'ANSRT') {
-            results[0].values.forEach((row,rdx) => {
-                cdres.alist.ansrt.push(row[cdx].split(',').map(Number));
-                while (cdres.alist.anset[rdx].length > optnum)
-                {
-                    let adel = Math.floor(Math.random()*cdres.alist.anset[rdx].length);
-                    let rdel = cdres.alist.ansrt[rdx].indexOf(adel+1);
-                    cdres.alist.anset[rdx].splice(adel,1);
-                    if (rdel != -1)
-                        cdres.alist.ansrt[rdx][rdel] = 0;
+    results.forEach(result => {
+        result.columns.forEach((col, cdx) => {
+            result.values.forEach((row, rdx) => {
+                switch (col) {
+                    case 'QCODE':
+                        cdres.qlist.qcode.push(row[cdx]);
+                        cdres.score.push(0);
+                        break;
+                    case 'QDISC':
+                        cdres.qlist.qdisc.push(row[cdx]);
+                        break;
+                    case 'QDESC':
+                        cdres.qlist.qdesc.push(row[cdx]);
+                        break;
+                    case 'ANSET':
+                        popTP_ANSET(row, cdx, rdx);
+                        break;
+                    case 'ANSRT':
+                        popTP_ANSRT(row, cdx, rdx);
+                        break;
                 }
             });
-        }
+        });
     });
-    return cdres;
+}
+
+function popTP_ANSET(row, cdx, rdx) {
+    cdres.alist.anset.push(row[cdx].slice(1,-1).replace(/","/g,'---').split('---'));
+    cdres.alist.arand.push(Array.from({length:optnum},() => Math.floor(Math.random() * 1000)));
+    cdres.alist.ansel.push(Array.from({length:optnum},() => 0));
+}
+
+function popTP_ANSRT(row, cdx, rdx) {
+    cdres.alist.ansrt.push(row[cdx].split(',').map(Number));
+    popTP_Trim(rdx);
+}
+
+function popTP_Trim(rdx) {
+    while (cdres.alist.anset[rdx].length > optnum) {
+        let adel = Math.floor(Math.random() * cdres.alist.anset[rdx].length);
+        if (adel !== cdres.alist.anset[rdx].length) cdres.alist.anset[rdx].splice(adel, 1);
+        let rdel = cdres.alist.ansrt[rdx].indexOf(adel);
+        if (rdel !== -1) cdres.alist.ansrt[rdx][rdel] = 0;
+    }
 }
 
 function createTable(qnum) {
@@ -94,13 +108,38 @@ function createTable(qnum) {
 function updateSelectedOptions(qnum) {
     const checkboxes = document.querySelectorAll(`input[class="ansel_${qnum}"]:checked`);
     cdres.alist.ansel[qnum] = Array.from(checkboxes).map(cb => parseInt(cb.id));
-    cdres.score[qnum] = cdres.alist.ansrt[qnum].filter(value => cdres.alist.ansel[qnum].includes(value)).length;
-    document.getElementById(`score_${qnum}`).innerHTML = `Question:${cdres.qlist.qcode[qnum]}<br>Score:${scoreCalc(qnum)}`;
+    cdres.score[qnum] = scoreCalc(qnum);
+    document.getElementById(`score_${qnum}`).innerHTML = `<br>Question code: ${cdres.qlist.qcode[qnum]}<br>Score: ${scoreCalc(qnum)}`;
+    qNumField(qnum);
 }
 
 function scoreCalc(qnum) {
-    const s = cdres.score[qnum];
+    const s = cdres.alist.ansrt[qnum].filter(value => cdres.alist.ansel[qnum].includes(value)).length;
     const sl = cdres.alist.ansel[qnum].length;
     const c = cdres.alist.ansrt[qnum].length;
     return ((s/c)-((sl-s)/(optnum - c)));
+}
+
+function qNumField(qnum) {
+    if (Number.isInteger(qnum)) {
+        const qnumText = document.getElementById('qbtn').innerHTML;
+        if (qnum < 20 && qnum > 0) {
+            switch(qnumText) {
+                case 'Next question':
+                    createTable(qnum);
+                    document.getElementById('qnum').value = qnum+1;
+                    break;
+                case 'Go to question':
+                    createTable(qnum-1);
+                    break;
+            }
+            document.getElementById('qbtn').innerHTML = "Next question";
+        }
+        if (qnum == 20 && qnumText == 'Next question') {
+            createTable(0);
+            document.getElementById('qbtn').innerHTML = "Go to question";
+        } 
+    } else {
+        document.getElementById('qnum').value = 1;
+    }
 }
